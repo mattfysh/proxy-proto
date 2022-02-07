2e620a87b66e1e24f399e24ad740c1c1303777d1
Matt Fysh <mattfysh@gmail.com>
2022-02-07 11:31:31 +1000

    use official caddy docker images

--- caddy/install.sh
+++
@@ -1,6 +0,0 @@
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | tee /etc/apt/trusted.gpg.d/caddy-xcaddy.asc
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-xcaddy.list
-apt update
-apt install xcaddy
-
-go get -d -v github.com/caddyserver/caddy/v2
---
+++ caddylocal/build.sh
@@ -0,0 +1,3 @@
+./xcaddy build \
+  --with github.com/mholt/caddy-l4=../../caddy-l4 \
+  --with github.com/abiosoft/caddy-yaml
